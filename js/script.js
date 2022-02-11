ddca9c924b380a52519b85986c27ea9579634753
Neimat Soliman <neimat.soliman.ismail@gmail.com>
2022-02-12 00:08:37 +0200

    client section end

--- js/script.js
+++ js/script.js
@@ -127,3 +127,27 @@ var swiper = new Swiper(".blogs-slider", {
         },
     },
 });
+
+
+var swiper = new Swiper(".clients-slider", {
+    loop:true,
+    grabCursor:true,
+    spaceBetween: 10,    
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            
+        },
+        640: {
+            slidesPerView: 2,
+            
+        },
+        768: {
+            slidesPerView: 3,
+            
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
+});
